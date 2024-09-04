a21438916760c0aee2ab1ae916918f932d6d298a
gino <gino@gino.com>
2024-09-04 06:16:46 +0000

    test: test case generated by GAI

--- packages/react/src/code/__tests__/Code.test.js
+++ packages/react/src/code/__tests__/Code.test.js
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
 import { render } from '@tonic-ui/react/test-utils/render';
 import { testA11y } from '@tonic-ui/react/test-utils/accessibility';
-import { Code } from '@tonic-ui/react/src';
+import Code from '@tonic-ui/react/src/code/Code';
 import React from 'react';
 
 describe('Code', () => {
@@ -16,9 +16,36 @@ describe('Code', () => {
     ), renderOptions);
 
     expect(screen.getByTestId('Code').tagName).toBe('CODE');
-
     expect(container).toMatchSnapshot();
-
     await testA11y(container);
+  });
+
+  it('should pass props correctly', () => {
+    const renderOptions = {
+      useCSSVariables: true,
+    };
+    const { container } = render((
+      <Code data-testid="Code" className="custom-class" id="custom-id">
+        Code
+      </Code>
+    ), renderOptions);
+
+    const codeElement = screen.getByTestId('Code');
+    expect(codeElement).toHaveClass('custom-class');
+    expect(codeElement).toHaveAttribute('id', 'custom-id');
+  });
+
+  it('should support additional styles', () => {
+    const renderOptions = {
+      useCSSVariables: true,
+    };
+    const { container } = render((
+      <Code data-testid="Code" style={{ color: 'red' }}>
+        Code
+      </Code>
+    ), renderOptions);
+
+    const codeElement = screen.getByTestId('Code');
+    expect(codeElement).toHaveStyle('color: red');
   });
 });
