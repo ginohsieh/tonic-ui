33e60977f2506921266343ed47dbde3fefcc21f4
gino <gino@gino.com>
2024-10-09 06:34:41 +0000

    test: test case generated by GAI

--- packages/react/src/alert/__tests__/Alert.test.js
+++ packages/react/src/alert/__tests__/Alert.test.js
@@ -44,3 +44,74 @@ describe('Alert', () => {
     });
   });
 });
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import AlertCloseButton from './AlertCloseButton';
+
+describe('AlertCloseButton', () => {
+  it('should render correctly', () => {
+    render(<AlertCloseButton />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should handle click events', async () => {
+    const handleClick = jest.fn();
+    const user = userEvent.setup();
+
+    render(<AlertCloseButton onClick={handleClick} />);
+    const button = screen.getByRole('button');
+    await user.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import AlertIcon from './AlertIcon';
+import { SuccessIcon } from '@tonic-ui/react-icons';
+
+describe('AlertIcon', () => {
+  it('should render default icon based on severity', () => {
+    render(<AlertIcon />);
+    const icon = screen.getByRole('img');
+    expect(icon).toBeInTheDocument();
+  });
+
+  it('should render custom icon', () => {
+    render(<AlertIcon icon={<SuccessIcon />} />);
+    const icon = screen.getByRole('img');
+    expect(icon).toBeInTheDocument();
+  });
+});
+
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import AlertMessage from './AlertMessage';
+
+describe('AlertMessage', () => {
+  it('should render correctly', () => {
+    const message = 'This is a test message';
+    render(<AlertMessage>{message}</AlertMessage>);
+    const messageElement = screen.getByText(message);
+    expect(messageElement).toBeInTheDocument();
+  });
+});
+
+import * as moduleExport from '@tonic-ui/react-icons/src';
+import * as icons from '@tonic-ui/react-icons/src/icons';
+
+test('should match expected exports', () => {
+  const expectedExports = [
+    'SVGIcon',
+    'createSVGIcon',
+    // icons
+    ...Object.keys(icons),
+  ];
+  const receivedExports = Object.keys(moduleExport);
+
+  expect(receivedExports.sort()).toEqual(expectedExports.sort());
+});
