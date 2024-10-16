1d1a8a7defb77014394ac8aab611bac360b75b4c
gino <gino@gino.com>
2024-10-16 07:31:14 +0000

    test: test case generated by GAI

--- packages/react-hooks/src/index.js
+++ packages/react-hooks/src/index.js
@@ -1,17 +1,67 @@
-export useConst from './useConst';
-export useCopyToClipboard from './useCopyToClipboard';
-export useEffectOnce from './useEffectOnce';
-export useEffectOnceWhen from './useEffectOnceWhen';
-export useEventCallback from './useEventCallback';
-export useEventListener from './useEventListener';
-export useHydrated from './useHydrated';
-export useIsomorphicEffect from './useIsomorphicEffect';
-export useLatest from './deprecated/useLatest'; // deprected: replaced by useLatestRef
-export useLatestRef from './useLatestRef';
-export useMediaQuery from './useMediaQuery';
-export useMergeRefs from './useMergeRefs';
-export useOnce from './useOnce';
-export useOnceWhen from './useOnceWhen';
-export useOutsideClick from './useOutsideClick';
-export usePrevious from './usePrevious';
-export useToggle from './useToggle';
+import { renderHook } from '@testing-library/react-hooks';
+import { useOnce } from './index';
+
+describe('useOnce', () => {
+  it('should be defined', () => {
+    expect(useOnce).toBeDefined();
+  });
+
+  it('should call the callback only once', () => {
+    const callback = jest.fn();
+
+    const { rerender } = renderHook(() => useOnce(callback));
+
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    // Rerender the hook
+    rerender();
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not call the callback on subsequent renders', () => {
+    const callback = jest.fn();
+
+    const { rerender, unmount } = renderHook(() => useOnce(callback));
+
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    // Rerender the hook multiple times
+    rerender();
+    rerender();
+    rerender();
+
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    // Unmount and remount the hook
+    unmount();
+    renderHook(() => useOnce(callback));
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should correctly handle an empty callback', () => {
+    const callback = jest.fn();
+
+    renderHook(() => useOnce(callback));
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should handle multiple instances separately', () => {
+    const callback1 = jest.fn();
+    const callback2 = jest.fn();
+
+    const { rerender: rerender1 } = renderHook(() => useOnce(callback1));
+    const { rerender: rerender2 } = renderHook(() => useOnce(callback2));
+
+    expect(callback1).toHaveBeenCalledTimes(1);
+    expect(callback2).toHaveBeenCalledTimes(1);
+
+    rerender1();
+    rerender2();
+
+    expect(callback1).toHaveBeenCalledTimes(1);
+    expect(callback2).toHaveBeenCalledTimes(1);
+  });
+});
