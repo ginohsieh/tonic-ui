16ed93805feee5921e51a63f387cd8c12a2b45de
gino <gino@gino.com>
2024-10-16 07:31:20 +0000

    test: test case generated by GAI

--- packages/react-hooks/src/index.js
+++ packages/react-hooks/src/index.js
@@ -1,17 +1,73 @@
-export useConst from './useConst';
-export useCopyToClipboard from './useCopyToClipboard';
-export useEffectOnce from './useEffectOnce';
-export useEffectOnceWhen from './useEffectOnceWhen';
-export useEventCallback from './useEventCallback';
-export useEventListener from './useEventListener';
-export useHydrated from './useHydrated';
-export useIsomorphicEffect from './useIsomorphicEffect';
-export useLatest from './deprecated/useLatest'; // deprected: replaced by useLatestRef
-export useLatestRef from './useLatestRef';
-export useMediaQuery from './useMediaQuery';
-export useMergeRefs from './useMergeRefs';
-export useOnce from './useOnce';
-export useOnceWhen from './useOnceWhen';
-export useOutsideClick from './useOutsideClick';
-export usePrevious from './usePrevious';
-export useToggle from './useToggle';
+import { renderHook } from '@testing-library/react-hooks';
+import { useEffectOnceWhen } from './index';
+import { useState } from 'react';
+
+describe('useEffectOnceWhen', () => {
+  it('should be defined', () => {
+    expect(useEffectOnceWhen).toBeDefined();
+  });
+
+  it('should call the callback when the condition is true', () => {
+    const callback = jest.fn();
+    renderHook(() => useEffectOnceWhen(callback, true));
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not call the callback when the condition is false', () => {
+    const callback = jest.fn();
+    renderHook(() => useEffectOnceWhen(callback, false));
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('should call the callback only once even if the condition changes to true again', () => {
+    const callback = jest.fn();
+    const { rerender } = renderHook(
+      ({ condition }) => useEffectOnceWhen(callback, condition),
+      {
+        initialProps: { condition: true },
+      }
+    );
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    rerender({ condition: false });
+    rerender({ condition: true });
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should work with dynamic conditions', () => {
+    const callback = jest.fn();
+    const { rerender } = renderHook(
+      ({ condition }) => useEffectOnceWhen(callback, condition),
+      {
+        initialProps: { condition: false },
+      }
+    );
+
+    expect(callback).not.toHaveBeenCalled();
+
+    rerender({ condition: true });
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should work with dependencies changing', () => {
+    const callback = jest.fn();
+    const { rerender } = renderHook(
+      ({ deps }) => useEffectOnceWhen(callback, deps.some(dep => dep)),
+      {
+        initialProps: { deps: [false, false] },
+      }
+    );
+
+    expect(callback).not.toHaveBeenCalled();
+
+    rerender({ deps: [true, false] });
+
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    rerender({ deps: [true, true] });
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
