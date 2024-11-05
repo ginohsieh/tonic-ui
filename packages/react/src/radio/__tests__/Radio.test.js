7698d9e0b498add73f4884be5d46a97996314a8b
gino <gino@gino.com>
2024-11-05 09:41:42 +0000

    test: test case generated by GAI

--- packages/react/src/radio/__tests__/Radio.test.js
+++ packages/react/src/radio/__tests__/Radio.test.js
@@ -1,31 +1,22 @@
-import { testA11y } from '@tonic-ui/react/test-utils/accessibility';
-import { render } from '@tonic-ui/react/test-utils/render';
-import { Radio } from '@tonic-ui/react/src';
+import { render, screen, fireEvent } from '@testing-library/react';
 import React, { useEffect, useRef } from 'react';
+import { Radio, RadioGroup, useRadioGroup } from '../index';
 
 describe('Radio', () => {
-  it('should render correctly', async () => {
-    const renderOptions = {
-      useCSSVariables: true,
-    };
-    const { container } = render((
+  it('should render correctly', () => {
+    const { container } = render(
       <>
-        {/* Sizes */}
-        <Radio size="sm" name="sizes">Label</Radio>
-        <Radio size="md" name="sizes">Label</Radio>
-        <Radio size="lg" name="sizes">Label</Radio>
+        <Radio size="sm" name="sizes">Small</Radio>
+        <Radio size="md" name="sizes">Medium</Radio>
+        <Radio size="lg" name="sizes">Large</Radio>
 
-        {/* States */}
-        <Radio defaultChecked={false}>Label</Radio>
-        <Radio defaultChecked>Label</Radio>
-        <Radio disabled>Label</Radio>
-        <Radio disabled defaultChecked>Label</Radio>
+        <Radio defaultChecked={false}>Unchecked</Radio>
+        <Radio defaultChecked>Checked</Radio>
+        <Radio disabled>Disabled</Radio>
+        <Radio disabled defaultChecked>Checked and Disabled</Radio>
       </>
-    ), renderOptions);
-
+    );
     expect(container).toMatchSnapshot();
-
-    await testA11y(container);
   });
 
   it('should render correctly with `inputRef` and `ref` props', () => {
@@ -47,8 +38,56 @@ describe('Radio', () => {
       );
     };
 
-    render(
-      <TestComponent />
+    render(<TestComponent />);
+  });
+});
+
+describe('RadioGroup', () => {
+  it('should render correctly', () => {
+    const { container } = render(
+      <RadioGroup defaultValue="option1">
+        <Radio value="option1">Option 1</Radio>
+        <Radio value="option2">Option 2</Radio>
+        <Radio value="option3">Option 3</Radio>
+      </RadioGroup>
     );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should call onChange when a radio button is clicked', () => {
+    const handleChange = jest.fn();
+    render(
+      <RadioGroup onChange={handleChange} defaultValue="option1">
+        <Radio value="option1">Option 1</Radio>
+        <Radio value="option2">Option 2</Radio>
+        <Radio value="option3">Option 3</Radio>
+      </RadioGroup>
+    );
+
+    fireEvent.click(screen.getByLabelText('Option 2'));
+    expect(handleChange).toHaveBeenCalledTimes(1);
+    expect(handleChange).toHaveBeenCalledWith('option2');
+  });
+
+  it('should update the selected value when props change', () => {
+    const { rerender } = render(
+      <RadioGroup value="option1">
+        <Radio value="option1">Option 1</Radio>
+        <Radio value="option2">Option 2</Radio>
+        <Radio value="option3">Option 3</Radio>
+      </RadioGroup>
+    );
+
+    expect(screen.getByLabelText('Option 1').checked).toBe(true);
+
+    rerender(
+      <RadioGroup value="option2">
+        <Radio value="option1">Option 1</Radio>
+        <Radio value="option2">Option 2</Radio>
+        <Radio value="option3">Option 3</Radio>
+      </RadioGroup>
+    );
+
+    expect(screen.getByLabelText('Option 2').checked).toBe(true);
   });
 });
