8a946dde53466ae4c9ada20d0f2dfc7dbaf688d3
gino <gino@gino.com>
2024-11-05 09:44:18 +0000

    test: test case generated by GAI

--- packages/react/src/button/__tests__/ButtonLink.test.js
+++ packages/react/src/button/__tests__/ButtonLink.test.js
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
-import { render } from '@tonic-ui/react/test-utils/render';
-import { testA11y } from '@tonic-ui/react/test-utils/accessibility';
-import { ButtonLink } from '@tonic-ui/react/src';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ButtonLink } from '../index';
 import React from 'react';
 
 describe('ButtonLink', () => {
@@ -15,12 +15,36 @@ describe('ButtonLink', () => {
         GitHub
       </ButtonLink>
     );
-    await testA11y(container);
+    expect(container).toMatchSnapshot();
   });
 
   it('verifies the link has the correct href value', () => {
     render(<ButtonLink href="/logout">Log out</ButtonLink>);
     const link = screen.getByRole('link', { name: /log out/i });
     expect(link.href).toBe('http://localhost/logout');
+  });
+
+  it('should call onClick handler when clicked', () => {
+    const handleClick = jest.fn();
+    render(
+      <ButtonLink href="https://example.com" onClick={handleClick}>
+        Click me
+      </ButtonLink>
+    );
+    const link = screen.getByRole('link', { name: /click me/i });
+    fireEvent.click(link);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should have the correct target attribute', () => {
+    render(<ButtonLink href="https://example.com" target="_blank">External</ButtonLink>);
+    const link = screen.getByRole('link', { name: /external/i });
+    expect(link).toHaveAttribute('target', '_blank');
+  });
+
+  it('should have the correct textDecoration style', () => {
+    render(<ButtonLink href="https://example.com" textDecoration="none">No Underline</ButtonLink>);
+    const link = screen.getByRole('link', { name: /no underline/i });
+    expect(link).toHaveStyle('text-decoration: none');
   });
 });
