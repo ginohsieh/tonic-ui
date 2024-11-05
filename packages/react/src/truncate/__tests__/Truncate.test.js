37344c711827a5e8b8b24467305263eaa1471ad8
gino <gino@gino.com>
2024-11-05 09:41:31 +0000

    test: test case generated by GAI

--- packages/react/src/truncate/__tests__/Truncate.test.js
+++ packages/react/src/truncate/__tests__/Truncate.test.js
@@ -1,20 +1,71 @@
-import { testA11y } from '@tonic-ui/react/test-utils/accessibility';
-import { render } from '@tonic-ui/react/test-utils/render';
+import { render } from '@testing-library/react';
 import { Truncate } from '@tonic-ui/react/src';
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('Truncate', () => {
-  it('should render correctly', async () => {
-    const renderOptions = {
-      useCSSVariables: true,
-    };
-    const { container } = render((
+  it('should render correctly', () => {
+    const { container } = render(
       <Truncate width={120}>
         This is a very long text that will be truncated
       </Truncate>
-    ), renderOptions);
+    );
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('should apply correct styles for truncation', () => {
+    const { getByText } = render(
+      <Truncate width={120}>
+        This is a very long text that will be truncated
+      </Truncate>
+    );
+
+    const textElement = getByText('This is a very long text that will be truncated');
+    expect(textElement).toHaveStyle({
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap',
+      width: '120px',
+    });
+  });
+
+  it('should handle empty content gracefully', () => {
+    const { container } = render(<Truncate width={120} />);
+
+    expect(container.firstChild).toBeEmptyDOMElement();
+  });
+
+  it('should handle different widths correctly', () => {
+    const { getByText, rerender } = render(
+      <Truncate width={100}>
+        This is a very long text that will be truncated
+      </Truncate>
+    );
+
+    let textElement = getByText('This is a very long text that will be truncated');
+    expect(textElement).toHaveStyle({
+      width: '100px',
+    });
+
+    rerender(
+      <Truncate width={200}>
+        This is a very long text that will be truncated
+      </Truncate>
+    );
+
+    textElement = getByText('This is a very long text that will be truncated');
+    expect(textElement).toHaveStyle({
+      width: '200px',
+    });
+  });
+
+  it('should meet accessibility standards', async () => {
+    const { container } = render(
+      <Truncate width={120}>
+        This is a very long text that will be truncated
+      </Truncate>
+    );
 
     await testA11y(container);
   });
