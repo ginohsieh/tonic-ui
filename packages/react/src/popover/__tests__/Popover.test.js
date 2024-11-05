f47f5a2f5f8ae5430f83261b0b13c711ec1d0560
gino <gino@gino.com>
2024-11-05 09:44:34 +0000

    test: test case generated by GAI

--- packages/react/src/popover/__tests__/Popover.test.js
+++ packages/react/src/popover/__tests__/Popover.test.js
@@ -1,9 +1,16 @@
-import { act, screen, waitFor } from '@testing-library/react';
+import { render, screen, act, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { render } from '@tonic-ui/react/test-utils/render';
 import { testA11y } from '@tonic-ui/react/test-utils/accessibility';
-import { Button, Popover, PopoverTrigger, PopoverContent } from '@tonic-ui/react/src';
-import React from 'react';
+import { Button } from '@tonic-ui/react/src/button';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+} from '@tonic-ui/react/src/popover';
 
 describe('Popover', () => {
   it('should render correctly', async () => {
@@ -11,7 +18,8 @@ describe('Popover', () => {
     const renderOptions = {
       useCSSVariables: true,
     };
-    const { container } = render((
+
+    const { container } = render(
       <Popover>
         <PopoverTrigger data-testid="popover-trigger">
           <Button>Hover me</Button>
@@ -19,8 +27,9 @@ describe('Popover', () => {
         <PopoverContent data-testid="popover-content">
           This is a popover
         </PopoverContent>
-      </Popover>
-    ), renderOptions);
+      </Popover>,
+      renderOptions
+    );
 
     const popoverTrigger = screen.getByTestId('popover-trigger');
     expect(popoverTrigger).toBeInTheDocument();
@@ -37,5 +46,88 @@ describe('Popover', () => {
     expect(container).toMatchSnapshot();
 
     await testA11y(container);
+  });
+
+  it('should handle close on trigger click', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Popover>
+        <PopoverTrigger data-testid="popover-trigger">
+          <Button>Click me</Button>
+        </PopoverTrigger>
+        <PopoverContent data-testid="popover-content">
+          This is a popover
+        </PopoverContent>
+      </Popover>
+    );
+
+    const popoverTrigger = screen.getByTestId('popover-trigger');
+
+    await act(() => user.click(popoverTrigger));
+    expect(await screen.findByTestId('popover-content')).toBeVisible();
+
+    await act(() => user.click(popoverTrigger));
+    await waitFor(() => expect(screen.queryByTestId('popover-content')).not.toBeInTheDocument());
+  });
+
+  it('should render with arrow', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Popover>
+        <PopoverTrigger data-testid="popover-trigger">
+          <Button>Click me</Button>
+        </PopoverTrigger>
+        <PopoverContent data-testid="popover-content">
+          <PopoverArrow />
+          This is a popover
+        </PopoverContent>
+      </Popover>
+    );
+
+    const popoverTrigger = screen.getByTestId('popover-trigger');
+
+    await act(() => user.click(popoverTrigger));
+
+    await waitFor(() => {
+      const popoverContent = screen.getByTestId('popover-content');
+      expect(popoverContent).toHaveTextContent('This is a popover');
+      expect(popoverContent.querySelector('[data-popper-arrow]')).toBeInTheDocument();
+    });
+  });
+
+  it('should support custom header, body, and footer', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Popover>
+        <PopoverTrigger data-testid="popover-trigger">
+          <Button>Click me</Button>
+        </PopoverTrigger>
+        <PopoverContent data-testid="popover-content">
+          <PopoverHeader data-testid="popover-header">Header</PopoverHeader>
+          <PopoverBody data-testid="popover-body">Body</PopoverBody>
+          <PopoverFooter data-testid="popover-footer">Footer</PopoverFooter>
+        </PopoverContent>
+      </Popover>
+    );
+
+    const popoverTrigger = screen.getByTestId('popover-trigger');
+
+    await act(() => user.click(popoverTrigger));
+
+    await waitFor(() => {
+      const popoverContent = screen.getByTestId('popover-content');
+      expect(popoverContent).toBeInTheDocument();
+
+      const popoverHeader = screen.getByTestId('popover-header');
+      const popoverBody = screen.getByTestId('popover-body');
+      const popoverFooter = screen.getByTestId('popover-footer');
+
+      expect(popoverHeader).toHaveTextContent('Header');
+      expect(popoverBody).toHaveTextContent('Body');
+      expect(popoverFooter).toHaveTextContent('Footer');
+    });
   });
 });
