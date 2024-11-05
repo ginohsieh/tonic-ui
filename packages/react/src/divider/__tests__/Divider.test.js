7a9a85c53df38e1cfc0b995a7d87886639862a5d
gino <gino@gino.com>
2024-11-05 09:45:20 +0000

    test: test case generated by GAI

--- packages/react/src/divider/__tests__/Divider.test.js
+++ packages/react/src/divider/__tests__/Divider.test.js
@@ -1,5 +1,5 @@
-import { testA11y } from '@tonic-ui/react/test-utils/accessibility';
-import { render } from '@tonic-ui/react/test-utils/render';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import { Divider, Stack } from '@tonic-ui/react/src';
 import React from 'react';
 
@@ -11,7 +11,7 @@ describe('Divider', () => {
     const { container } = render((
       <>
         <Stack direction="column" spacing="4x">
-          <Divider variand="solid" orientation="horizontal" />
+          <Divider variant="solid" orientation="horizontal" />
           <Divider variant="dashed" orientation="horizontal" />
           <Divider variant="dotted" orientation="horizontal" />
         </Stack>
@@ -24,7 +24,46 @@ describe('Divider', () => {
     ), renderOptions);
 
     expect(container).toMatchSnapshot();
+  });
 
-    await testA11y(container);
+  it('should render with different variants', () => {
+    const { getByTestId } = render(
+      <>
+        <Divider data-testid="solid-divider" variant="solid" />
+        <Divider data-testid="dashed-divider" variant="dashed" />
+        <Divider data-testid="dotted-divider" variant="dotted" />
+      </>
+    );
+
+    expect(getByTestId('solid-divider')).toHaveStyle('border-style: solid');
+    expect(getByTestId('dashed-divider')).toHaveStyle('border-style: dashed');
+    expect(getByTestId('dotted-divider')).toHaveStyle('border-style: dotted');
+  });
+
+  it('should render with different orientations', () => {
+    const { getByTestId } = render(
+      <>
+        <Divider data-testid="horizontal-divider" orientation="horizontal" />
+        <Divider data-testid="vertical-divider" orientation="vertical" />
+      </>
+    );
+
+    expect(getByTestId('horizontal-divider')).toHaveStyle('border-width: 0 0 1px 0');
+    expect(getByTestId('vertical-divider')).toHaveStyle('border-width: 0 1px 0 0');
+  });
+
+  it('should accept and apply custom attributes', () => {
+    const { getByTestId } = render(
+      <Divider data-testid="custom-divider" aria-label="Custom Divider" />
+    );
+
+    expect(getByTestId('custom-divider')).toHaveAttribute('aria-label', 'Custom Divider');
+  });
+
+  it('should match the snapshot', () => {
+    const { asFragment } = render(
+      <Divider variant="solid" orientation="horizontal" />
+    );
+    expect(asFragment()).toMatchSnapshot();
   });
 });
